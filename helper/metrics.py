f661891f373bc67a1e8c7fe1b8435f5ca4aecbd2
schuelie <84412807+schuelie@users.noreply.github.com>
2024-12-20 04:07:27 +0100

    added average_distance in metrics.py

--- helper/metrics.py
+++ helper/metrics.py
@@ -1,24 +1,26 @@
 import time
-import json
 import pandas as pd
 import numpy as np
-from algorithm import AStarAlgorithm, WavefrontAlgorithm, MeetInTheMiddleAlgorithm, SASAR, QLearning
+from algorithm import AStarAlgorithm, WavefrontAlgorithm, MeetInTheMiddleAlgorithm
 
-from simulation import CreateGrid, GridWorldEnv
+from simulation import CreateGrid
 
 benchmarks = ['empty', 'star_grid', 'simple_grid', 'bottleneck_grid', 'trap_grid', 'doubletrap_grid', 'detour_grid']
+
 
 class Metrics:
 
     def get_metrics(self, algorithm: str = False, size_grid: int = 32, num_runs: int = 5):
         alg = None
 
-        measurements = pd.DataFrame(columns=['benchmark', 'avg_time (sec)', 'avg_path_length', 'avg_steps_to_converge'])
+        measurements = pd.DataFrame(columns=['benchmark', 'avg_time (sec)', 'avg_path_length', 'avg_steps_to_converge'
+                                                                                               'avg_distance'])
 
         # measurements for each algorithm
         all_times = []
-        all_path_lengths =[]
+        all_path_lengths = []
         all_steps = []
+        all_distances = []
 
         if algorithm == 'A-star':
             alg = AStarAlgorithm()
@@ -34,6 +36,7 @@ class Metrics:
             times = []
             path_lengths = []
             steps = []
+            distances = []
 
             for i in range(num_runs):
                 grid_creator = CreateGrid()
@@ -45,6 +48,7 @@ class Metrics:
 
                 s = time.perf_counter()
                 path, step_used = alg.find_path(start, end, grid)
+                avg_distance = self.average_distance_to_obstacles(path, grid)
 
                 # returns false if no path could be found
                 if not path:
@@ -55,15 +59,17 @@ class Metrics:
                 times.append(time_used)
                 path_lengths.append(len(path))
                 steps.append(step_used)
+                distances.append(avg_distance)
 
+            all_distances.extend(distances)
             all_times.extend(times)
             all_path_lengths.extend(path_lengths)
             all_steps.extend(steps)
 
-            row = pd.DataFrame([[b,times, path_lengths, steps]], columns=measurements.columns)
+            row = pd.DataFrame([[b, times, path_lengths, steps]], columns=measurements.columns)
             measurements = pd.concat([measurements, row], ignore_index=True)
 
-        row = pd.DataFrame([['all', all_times, all_path_lengths, all_steps]], columns=measurements.columns)
+        row = pd.DataFrame([['all', all_times, all_path_lengths, all_steps, all_distances]], columns=measurements.columns)
         measurements = pd.concat([measurements, row], ignore_index=True)
 
         df_new = measurements.iloc[:, 1:]
@@ -72,54 +78,33 @@ class Metrics:
 
         return measurements
 
-    def get_rl_metrics(self,
-                       algorithm: str = False,
-                       size_grid: int = 32,
-                       n_train_epochs = [500,1000,10000],
-                       learning_rate: int =0.7,
-                       gamma: int = 0.95,
-                       max_epsilon: int = 1.0,
-                       min_epsilon: int = 0.05,
-                       decay_rate: int = 0.0005,
-                       n_eval_episodes=100
-                       ):
+    def average_distance_to_obstacles(self, path, grid):
+        # alle blockierten zellen finden
+        blocked_indices = np.argwhere(grid)
 
-        measurements = pd.DataFrame(columns=['benchmark', 'train_epoch' ,'avg_time_train (sec)', 'mean_reward', 'std_reward'])
+        if blocked_indices.size == 0:
+            # keine hindernisse >> distanz ist unendlich
+            return [float('inf') for _ in path]
 
-        for b in benchmarks:
+        distances = []
+        for position in path:
+            x, y = position
+            # sind innerhalb des gitters
+            if 0 <= x < grid.shape[0] and 0 <= y < grid.shape[1]:
+                # manhatten distanz zu den hindernissen
+                dists = np.abs(blocked_indices[:, 0] - x) + np.abs(blocked_indices[:, 1] - y)
+                avg_dist = np.mean(dists)
+                distances.append(avg_dist)
+            else:
+                distances.append(float('inf'))
+                print("Befindet sich außerhalb des Grids.")
 
-            for i in n_train_epochs:
-                grid_creator = CreateGrid()
-                grid_dict = grid_creator.create_grid(b, size_grid)
-
-                env = GridWorldEnv(render_mode="rgb_array", map_dict=grid_dict)
-
-                if algorithm == 'sasar':
-                    alg = SASAR(env)
-                elif algorithm == 'qlearning':
-                    alg = QLearning(env)
-                else:
-                    print(f"Algorithm {algorithm} not found")
-                    return False
-
-                max_steps = alg.n_states * 2
-
-                s = time.perf_counter()
-                q_table = alg.train(i,min_epsilon, max_epsilon, decay_rate, max_steps, learning_rate, gamma)
-                e = time.perf_counter()
-                time_used = e - s
-
-                mean, std  = alg.evaluate_agent(max_steps, n_eval_episodes)
-
-                row = pd.DataFrame([[b, i , time_used, mean, std]], columns=measurements.columns)
-                measurements = pd.concat([measurements, row], ignore_index=True)
-
-        return measurements
-
-
-
-
-
+        # durschnitt über den ganzen pfad >> keine unendichen distanzen
+        all_distances = [d for d in distances if np.isfinite(d)]
+        if all_distances:
+            return np.mean(all_distances)
+        else:
+            return float('inf')
 
 
 def mean_std(x):
